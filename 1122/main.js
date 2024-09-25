f4b7716cdddca7a76c5d4f3118ccad86dd737de4
yj0206 <2452665078@qq.com>
2024-09-25 20:08:11 +0800

    Add files via upload

---
+++ 1122/main.js
@@ -0,0 +1,14 @@
+// 设置画布
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+// 生成随机数的函数
+
+function random(min,max) {
+  const num = Math.floor(Math.random() * (max - min)) + min;
+  return num;
+}
